aceabaacbd0f0c4ba8b6d5a04f20b0ba82f45c5e
jackofall122 <21017005@myrp.edu.sg>
2023-04-26 16:09:00 +0800

    test1

--- app/src/main/java/sg/edu/rp/c346/id21017005/prototypee/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21017005/prototypee/MainActivity.java
@@ -1,14 +1,56 @@
 package sg.edu.rp.c346.id21017005.prototypee;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REUEST_CODE = 22;
+    Button btnpicture;
+    ImageView imageView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnpicture = findViewById(R.id.btn1);
+        imageView = findViewById(R.id.imageview);
+
+        btnpicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REUEST_CODE && resultCode == RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+        else{
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
